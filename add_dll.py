a9cbccc8336a12ee2f27161eaa4dbd383c11081f
tildejustin <tildejustin@gmail.com>
2026-09-10 23:03:21 -0400

    prettify add dll

--- add_dll.py
+++ add_dll.py
@@ -1,31 +1,29 @@
+import argparse
 import csv
 import hashlib
 import pathlib
 import sys
 
-output = "legal-dlls.csv"
-
+output = pathlib.Path("legal-dlls.csv")
 
 with open(output, "r") as f:
     reader = csv.reader(f)
     fields = next(reader)
     curr = list(reader)
 
-assert len(sys.argv) == 3
-filename = sys.argv[1]
-version = sys.argv[2]
-file = pathlib.Path(filename)
+parser = argparse.ArgumentParser("add dll")
+parser.add_argument("file", type=pathlib.Path)
+parser.add_argument("version")
+args = parser.parse_args()
+file = args.file
 if not file.exists():
     print(f"{file}: file does not exist!", file=sys.stderr)
     exit(1)
 
-
-with open(file, "rb") as f:
-    name = file.name
-    checksum = hashlib.sha512(f.read()).hexdigest()
-    print(f"adding {name}: {checksum}")
-    curr.append([name, version, checksum])
-
+name = file.name
+checksum = hashlib.sha512(file.read_bytes()).hexdigest()
+print(f"adding {name}: {checksum}")
+curr.append([name, args.version, checksum])
 
 with open(output, "w", newline="") as f:
     writer = csv.writer(f, lineterminator="\n")
